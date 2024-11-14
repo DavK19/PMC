6b86f2fa2d97c58a5086a7c8e9f6979cf13e95df
DavK19 <jd.camargop1@uniandes.edu.co>
2024-11-14 12:17:42 -0500

    Actualización de la base de datos y modelos de eventos
    - Modificado: backend/eventos/admin.py
    - Modificado: backend/eventos/models.py
    - Añadido: backend/eventos/fixtures/eventos_fixture.json
    - Añadido: backend/eventos/migrations/0002_remove_evento_ubicacion_evento_latitud_and_more.py
    - Modificado: backend/db.sqlite3
    
    Se han realizado cambios en los modelos de eventos y se ha actualizado la base de datos en consecuencia. También se han añadido fixtures para los eventos.

--- backend/eventos/admin.py
+++ backend/eventos/admin.py
@@ -4,5 +4,6 @@ from .models import Evento
 # Register your models here.
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'fecha', 'ubicacion')
-    search_fields = ('nombre', 'ubicacion')
+    list_display = ('nombre', 'fecha', 'latitud', 'longitud')  # Usar los campos correctos
+    search_fields = ('nombre', 'descripcion')
+    list_filter = ('fecha',)
